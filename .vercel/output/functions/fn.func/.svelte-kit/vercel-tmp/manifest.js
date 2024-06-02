9f098b2d3b9ca1d747dce790ee3605e731c092bd
danieleavolio <daniele.avolio14@gmail.com>
2024-06-02 19:23:04 +0100

    Update

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","bachelor-thesis.pdf","CV-DA.pdf","favicon-16x16.png","favicon-32x32.png","favicon.ico","favicon.png","files/q&a.pdf","images/avatar.jpg","images/avatar.png","images/dark1.png","images/nierui.png","images/pattern.png","robots.txt","site.webmanifest"]),
 	mimeTypes: {".png":"image/png",".pdf":"application/pdf",".ico":"image/vnd.microsoft.icon",".jpg":"image/jpeg",".txt":"text/plain",".webmanifest":"application/manifest+json"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.2e94d2c5.js","app":"_app/immutable/entry/app.b9595cea.js","imports":["_app/immutable/entry/start.2e94d2c5.js","_app/immutable/chunks/scheduler.40fa21c2.js","_app/immutable/chunks/singletons.07ebf34d.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.b9595cea.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.40fa21c2.js","_app/immutable/chunks/index.babe5fbe.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.9af69918.js","app":"_app/immutable/entry/app.77a99823.js","imports":["_app/immutable/entry/start.9af69918.js","_app/immutable/chunks/scheduler.40fa21c2.js","_app/immutable/chunks/singletons.3166469a.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.77a99823.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.40fa21c2.js","_app/immutable/chunks/index.babe5fbe.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
